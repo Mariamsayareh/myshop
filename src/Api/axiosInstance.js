be52a491e77307288bdc28651a741151536fbfb7
Mariamsayareh <mariamsayarah@gmail.com>
2026-01-18 14:04:43 +0200

    header

--- src/Api/axiosInstance.js
+++ src/Api/axiosInstance.js
@@ -3,4 +3,8 @@ import axios from "axios";
 const axiosInstance = axios.create({
     baseURL: 'https://knowledgeshop.runasp.net/api'
 });
+axiosInstance.interceptors.request.use((config) => {
+    config.headers["Accept-Language"] = "en"
+    return config; //بحمل كل معلومات الريكوست
+})
 export default axiosInstance;
