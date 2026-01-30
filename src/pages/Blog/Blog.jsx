4b3df6c59b7422ffcd79b8df67282417bf43e529
Mariamsayareh <mariamsayarah@gmail.com>
2026-01-30 15:06:19 +0200

    blog page is done

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import HeroSec from '../../commponrnts/Hero/HeroSec';
 import {
   Box,
-  Typography,
+  Grid, 
+  Container,
   Link,
 } from "@mui/material";
 import { Link as RouterLink, useNavigate } from "react-router-dom";
+import CardItem from "../Card/CardItem.jsx";
+import { cards } from "../Card/Cards.js";
 
 const Blog = () => {
     return (
@@ -24,7 +27,19 @@ const Blog = () => {
                    / Blog
                   </Link>   / Blog
             </HeroSec>
-            jjj
+            <Container my={4}>
+                <Grid container spacing={4} my={8}>
+                    {cards.map((card) => (
+                    <Grid item xs={12} md={4} key={card.id} 
+                    sx={{
+                        width: { xs: "100%", sm: "60%", md: "30%" }, 
+                        
+                    }}>
+                        <CardItem card={card} />
+                    </Grid>
+                    ))}
+                </Grid>
+            </Container>
         </Box>
     );
 }
